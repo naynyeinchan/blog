29def5ffbf8749f192a1c60c24d4480d06293243
naynyeinchan <naychan700@gmail.com>
2022-06-19 21:22:05 +0630

    laravel blog

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Auth::routes();
+
+Route::resource('/blogs',\App\Http\Controllers\BlogController::class);
